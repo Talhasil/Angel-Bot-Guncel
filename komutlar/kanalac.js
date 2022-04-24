20fccb2fb4bbd3c0a8b5b3cdb82c5e682377bca5
Glitch (better-heliotrope-grey) <none>
2022-04-24 22:08:08 +0000

    🎷😽 Checkpoint ./komutlar/kanalac.js:43950496/213

--- komutlar/kanalac.js
+++ komutlar/kanalac.js
@@ -3,12 +3,12 @@ name:"kanal-aç",
 code:`
 $author[1;Kanal Oluşturuldu;$userAvatar]
 $description[1;
-<@$authorID> Tarafından <#$messageSlice[1;10]> Adlı Kanal Oluşturuldu]
+<@$authorID> Tarafından #$messageSlice[1;10] Adlı Kanal Oluşturuldu]
 $thumbnail[1;$userAvatar]
 $footer[1;$username Kullandı]
 $color[1;RANDOM]
 $addTimestamp[1;$datesTamp]
-$createChannel[$guildID;$messageSlice[1;10];text;;$message[1]]
-$onlyIf[$hasRoles[$guildID;$authorID;940931380106379295]==true;sg]
-
+$createChannel[$guildID;$messageSlice[1;10];text;;$channelID[$message[1]]]
+$cooldown[5m;Sayın Yetkili 5 Dakikada Bir Kod Ekleyebilirsin]
+$onlyIf[$hasRoles[$guildID;$authorID;940931380106379295]==true;Yetkin Yok Güzelim]
 `} 
