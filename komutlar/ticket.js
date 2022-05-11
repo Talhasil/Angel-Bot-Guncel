e3a3c15013ffc4d0696f41c4bf73b9a9258ec2bc
Glitch (better-heliotrope-grey) <none>
2022-05-11 12:31:30 +0000

    ☺️🚪 Checkpoint ./komutlar/ticket.js:43950496/1479

--- komutlar/ticket.js
+++ komutlar/ticket.js
@@ -2,7 +2,12 @@ module.exports = [{
   name:"ticket",
   code:`
   $addButton[1;Ticket Aç;2;ticketaç;no;🎫]
-  🎫 Ticket açmak için aşağıdaki butona basmanız yeterli. Kod yardımı abone ss vb. için ticket açmazsanız seviniriz. ( Boşa ticket açanlar 30 dakika mutelenicek. )
+Ticket açmak için aşağıdaki butona basmanız yeterlidir.
+
+Yetkili başvurusu,reklam yaptırma,bot yaptırma vb. şeyler için ticket açabilirsiniz.
+
+Kod yardımı,abone ss vb. için ticket açmazsanız seviniriz.
+( Uymayanlar ve boşa ticket açanlar 3 saat mutelenecektir )
   $onlyForIDs[$botOwnerID;]
   `
   },{
@@ -13,7 +18,7 @@ module.exports = [{
     $interactionReply[🎫 Ticket'ınız açıldı. <#$get[ticket]>;;;;;yes]
     $setUserVar[tticket;$get[ticket]]
     $setChannelVar[tuser;$authorID;$get[ticket]]
-    $let[ticket;$newTicket[・ticket・$getServerVar[tsayı];$getServerVar[tick] <@$authorID>, Ticket'ınız açıldı. Üst yetkililer birazdan sizinle ilgilenicek.;$getServerVar[tkategori];yes;uups]]
+    $let[ticket;$newTicket[ticket・$getServerVar[tsayı];$getServerVar[tick] <@$authorID>, Ticket'ınız açıldı. Üst yetkililer birazdan sizinle ilgilenicek.;$getServerVar[tkategori];yes;uups]]
     $wait[1s]
     $setServerVar[tsayı;$sum[$getServerVar[tsayı];1]]
     $onlyIf[$getUserVar[tticket]==;{"content": "Zaten bir ticket'ın var. <#$getUserVar[tticket]>","ephemeral": true, "options":{"interaction": true }}]
