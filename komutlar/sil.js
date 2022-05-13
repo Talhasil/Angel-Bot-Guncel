87c4bf4e40da5d7fba83179146fd9071c926a7e6
Glitch (better-heliotrope-grey) <none>
2022-05-13 05:28:48 +0000

    👘🐕 Checkpoint ./komutlar/sil.js:43950496/1354 ./komutlar/kanalunlock.js:43950496/966 ./komutlar/kanallock.js:43950496/964

--- komutlar/sil.js
+++ komutlar/sil.js
@@ -1,9 +1,10 @@
 module.exports = {
-   name: "sil",
-   code: `
+  name: "sil",
+  code: `
 $clear[$message[1]]
 $onlyPerms[managemessages;Bu komutu kullanmak için ”Mesajları Yönet“ yetkisine sahip olman gerek.]
 **$message** adet mesaj başarıyla havaya uçuruldu!
-$onlyIf[$isNumber[$message[1]]!=false;Lütfen sileceğin mesaj miktarını gir. 🔴]
+$onlyIf[$isNumber[$message[1]]!=false;Lütfen sileceğin mesaj miktarını gir. $getServerVar[cross]]
 $deleteIn[3s]
-`}
+`,
+};
