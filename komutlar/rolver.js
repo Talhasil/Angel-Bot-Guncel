57e9b9d1946736b6ce35c9184ba381b98673dff8
Glitch (better-heliotrope-grey) <none>
2022-05-21 13:00:17 +0000

    🐘🔦 Checkpoint ./komutlar/rolver.js:43950496/242

---
+++ komutlar/rolver.js
@@ -0,0 +1,15 @@
+module.exports = [{
+    name: "rolver",
+    code: `
+$forEachMember[1;{};deneme;]
+
+`
+    },{
+    name: "deneme",
+    type: "awaited",
+    code: `
+    
+   $giveRole[$guildID;$authorID;977507918746365972] 
+  $onlyIf[$isBot==true;] 
+    `
+    }]
