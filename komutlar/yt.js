876c0e71bf5c2571d9ab2f3590932d340e759d3b
Glitch (better-heliotrope-grey) <none>
2022-05-26 05:41:09 +0000

    🎷🌓 Checkpoint ./komutlar/yt.js:45205821/13 ./komutlar/site.js:45205821/2

--- komutlar/yt.js
+++ komutlar/yt.js
@@ -2,6 +2,6 @@ module.exports = {
   name:"yt",
   code:`
   $addButton[1;Tıkla!;link;https://youtube.com/channel/UC_PyS_CVMkEfXCrwg927p1g;no]
-  $getServerVar[tick] Youtube Kanalımız
+  $getServe$getServerVarrVar[tick] Youtube Kanalımız
   `
 }
